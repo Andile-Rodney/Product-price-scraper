cd404953b5b3c35cacc7967f58755f3787201604
MthunziNamba <rodneymthunzi@gmail.com>
2019-07-19 05:10:24 +0200

    Updated the parser.

--- scrapper.py
+++ scrapper.py
@@ -4,17 +4,19 @@ import smtplib
 
 URL = 'https://www.amazon.com/Sony-Full-Frame-Mirrorless-Digital-28-70mm/dp/B00FRDV06I/ref=sr_1_3?crid=2AZ92TRSHFZQ3&keywords=sony+a7&qid=1563390623&s=electronics&sprefix=sony+a7%2Celectronics%2C444&sr=1-3'
 
-headers = {"User-Agent": Mozilla/5.0 (Windows NT 10.0
-                                      Win64
-                                      x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36}
+headers = {"User-Agent": Mozilla/5.0 (Windows NT 10.0, Win64, x64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/75.0.3770.142 Safari/537.36}
+
+headers = {'Cache-Control': 'no-cache', "Pragma": "no-cache"}
 
 def check_price():
     page = requests.get(URL, headers=headers)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup1 = BeautifulSoup(page.content, 'html.parser')
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
 
-    title = soup.find(id="productTitle").get_text()
-    price = soup.find(id="priceblock_ourprice").get_text()
+
+    title = soup2.find(id="productTitle").get_text()
+    price = soup2.find(id="priceblock_ourprice").get_text()
     converted_price = float(price[0:5])
 
     if(converted_price < 1.700):
